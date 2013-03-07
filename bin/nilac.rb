d770128df89f6db3f7868fc8ec8d2af1392b2a1a
Adhithya Rajasekaran <adhithyan15@gmail.com>
2013-03-07 17:23:52 -0500

    added gitignore file to ignore binary files

--- bin/nilac.rb
+++ bin/nilac.rb
@@ -714,16 +714,6 @@ def create_executable(input_file)
 
   windows_output = `ocra --add-all-core #{input_file}`
 
-  mac_file_contents = ["#!/usr/bin/env ruby"] + read_file_line_by_line(input_file)
-
-  mac_file_path = input_file.sub(".rb","")
-
-  file_id = open(mac_file_path,"w")
-
-  file_id.write(mac_file_contents.join)
-
-  file_id.close
-
 end
 
 def create_mac_executable(input_file)
@@ -775,7 +765,7 @@ OptionParser.new do |opts|
 
   end
 
-  opts.on("-bmac", "--buildmac FILE", "Builds Mac Executables") do |file|
+  opts.on("-m", "--buildmac FILE", "Builds Mac Executables") do |macfile|
 
     file_path = Dir.pwd + "/nilac.rb"
 
