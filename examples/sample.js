36a35897a134c4aa696e170bf8da5e229e7287d7
Adhithya Rajasekaran <adhithyan15@gmail.com>
2013-03-07 13:37:47 -0500

    Added support for named functions

---
+++ examples/sample.js
@@ -0,0 +1,50 @@
+//Written in Nila and compiled into Javascript.Have fun!
+
+//Visit http://adhithyan15.github.com/nila to know more!
+
+var hello, message, goal_reached;
+
+
+//This is a very basic Nila file. 
+
+hello = "world";
+
+
+message = "Welcome to our webpage";
+
+
+goal_reached = 72;
+
+
+function printsquare(input_number) {
+//This is a very simple Nila function
+
+var add_number;
+
+
+	add_number = input_number + 10;
+
+
+	function square(number) {
+
+		return number*number;
+
+
+	}
+
+
+	console.log(square(add_number));
+
+
+}
+
+
+printsquare(5);
+
+
+function printer() {
+
+	console.log(hello +  world);
+
+
+}
