9283ee6a9a1b6fcc3ac354c3e8df4d65808669a9
Adhithya Rajasekaran <adhithyan15@gmail.com>
2013-03-30 03:30:14 -0400

    Resolved all the conflicts due to merging

--- examples/sample.js
+++ examples/sample.js
@@ -4,11 +4,7 @@
 
 (function() {
   
-<<<<<<< HEAD
   var hello, msg, message, long_passage, goal_reached, isprime, names;
-=======
-  var hello, msg, message, goal_reached, isprime, names;
->>>>>>> f4687fd26e3c7520a3ff7f1c01d6d7d746482596
   
   
   hello = "world";
@@ -17,15 +13,10 @@
   msg = "Nila";
   
   
-<<<<<<< HEAD
   message = "Welcome to " + msg;
   
   
   long_passage = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor \nincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \nexercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute \nirure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \npariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia\ndeserunt mollit anim id est laborum.";
-=======
-  
-  message = "Welcome to " + msg;
->>>>>>> f4687fd26e3c7520a3ff7f1c01d6d7d746482596
   
   
   goal_reached = 72;
@@ -72,10 +63,7 @@
     
     
   }
-<<<<<<< HEAD
   
-=======
->>>>>>> f4687fd26e3c7520a3ff7f1c01d6d7d746482596
 
 
 }).call(this);
